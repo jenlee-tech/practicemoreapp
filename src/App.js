baa23495a6a7dc1cb157ffb925aaadc5ce791d3a
Jennifer Lee <jenlee.tech@gmail.com>
2022-03-12 17:57:48 -0500

    added Button button for nicer styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
+import Button from "./components/UI/Button/Button";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
@@ -11,8 +12,8 @@ function App() {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <button onClick={handleClickButton}>Click me</button>
       {showParagraph && <p>The state has been clicked positive</p>}
+      <Button onClick={handleClickButton}>Click Me</Button>
     </div>
   );
 }
