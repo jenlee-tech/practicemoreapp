f94e16d056f6a4195ec623aae1643da42a097689
Jennifer Lee <jenlee.tech@gmail.com>
2022-03-18 00:25:50 -0400

    applied memo method on Button and DemoOutput components

--- src/components/Demo/DemoOutput.js
+++ src/components/Demo/DemoOutput.js
@@ -1,6 +1,8 @@
 import React from "react";
 
-export default function DemoOutput(props) {
+function DemoOutput(props) {
   console.log("evaluating the demooutput");
   return <p>{props.show ? "This is a paragraph" : ""}</p>;
 }
+
+export default React.memo(DemoOutput);
