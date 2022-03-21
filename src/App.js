762534bccd91be95c904973352eb152a484383c7
Jennifer Lee <jenlee.tech@gmail.com>
2022-03-20 22:59:36 -0400

    tested a dependency on the usecallback function for handleClickButton

--- src/App.js
+++ src/App.js
@@ -5,18 +5,29 @@ import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   const handleClickButton = useCallback(() => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  }, []); //useCallBack needs a dependency.  When useCallback is used, the same function is used in memory and not re-evaluated everytime the state changes - it is similiar to the memo method where the prop is not re-evaluated.
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  //useCallBack needs a dependency.  When useCallback is used, the same function is used in memory and not re-evaluated everytime the state changes (this includes variables too - unless you place a dependency which will recreate the function) - it is similiar to the memo method where the prop is not re-evaluated. (primitive reference concept and closures)
 
   console.log("re-evaluating the entire function");
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <Button onClick={handleClickButton}>Click Me</Button>
-      <DemoOutput show={false}></DemoOutput>
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
+      <Button onClick={handleClickButton}>Toggle Paragraph</Button>
+
+      <DemoOutput show={showParagraph}></DemoOutput>
     </div>
   );
 }
