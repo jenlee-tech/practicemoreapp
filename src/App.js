3d7c57d0b53b38017b05c8f5beb9e96b172e580c
Jennifer Lee <jenlee.tech@gmail.com>
2022-03-25 16:21:45 -0400

    more comments on states and when the components get rendered

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import DemoOutput from "./components/Demo/DemoOutput";
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
   const [allowToggle, setAllowToggle] = useState(false);
-  //state is only initalized once when the component initially evaluated.  After that, it will note the difference in state but not re-initialized (unless, it was taken off the DOM and the application starts again)
+  //state is only initalized once when the component initially evaluated and it is associated with a particular component.  After that, it will note the difference in state but not re-initialized (unless, it was taken off the DOM and the application starts again)
 
   const handleClickButton = useCallback(() => {
     if (allowToggle) {
       setShowParagraph((prevShowParagraph) => !prevShowParagraph);
     }
   }, [allowToggle]);
+
+  //when updating two different states and the code is synchronous, both will be in the same process at the same time but its not asynchronous nor does one state gets evaluated and then rendered.
 
   //useCallBack needs a dependency.  When useCallback is used, the same function is used in memory and not re-evaluated everytime the state changes (this includes variables too - unless you place a dependency which will recreate the function) - it is similiar to the memo method where the prop is not re-evaluated. (primitive reference concept and closures)
 
